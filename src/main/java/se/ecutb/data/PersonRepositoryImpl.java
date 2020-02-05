bd438f58bdf7837061411e41ab0ae0bf20af8de2
Peter Sundberg <peter.sundberg67@gmail.com>
2020-02-05 10:09:40 +0100

    PersonRepositoryImpl and TodoRepositoryImpl done.

--- src/main/java/se/ecutb/data/PersonRepositoryImpl.java
+++ src/main/java/se/ecutb/data/PersonRepositoryImpl.java
@@ -3,59 +3,73 @@ package se.ecutb.data;
 import se.ecutb.model.Address;
 import se.ecutb.model.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class PersonRepositoryImpl implements PersonRepository{
 
+    private List<Person> people = new ArrayList<>();
 
     @Override
     public Optional<Person> findById(int personId) {
-        return Optional.empty();
+
+        return people.stream()
+                .filter(person -> person.getPersonId() == personId)
+                .findFirst();
     }
 
     @Override
     public Person persist(Person person) throws IllegalArgumentException {
-        return null;
+        if(people.contains(person)){
+            throw new IllegalArgumentException("Duplicate email");
+        }
+            people.add(person);
+            return person;
     }
 
     @Override
     public Optional<Person> findByEmail(String email) {
-        return Optional.empty();
+
+        return people.stream().filter(person -> person.getEmail().equals(email)).findFirst();
     }
 
     @Override
     public List<Person> findByAddress(Address address) {
-        return null;
+        return people.stream().filter(person -> person.getAddress().equals(address))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Person> findByCity(String city) {
-        return null;
+        return people.stream().filter(person -> person.getAddress().getCity().equals(city))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Person> findByLastName(String lastName) {
-        return null;
+        return people.stream().filter(person -> person.getLastName().equalsIgnoreCase(lastName)).collect(Collectors.toList());
     }
 
     @Override
     public List<Person> findByFullName(String fullName) {
-        return null;
+        return people.stream().filter(person -> (person.getFirstName() + " " + person.getLastName()).equalsIgnoreCase(fullName))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Person> findAll() {
-        return null;
+        return people;
     }
 
     @Override
     public boolean delete(int personId) throws IllegalArgumentException {
-        return false;
+        return people.remove(findById(personId).orElseThrow(IllegalArgumentException::new));
     }
 
     @Override
     public void clear() {
-
+        people.clear();
     }
 }
--- src/main/java/se/ecutb/data/TodoRepositoryImpl.java
+++ src/main/java/se/ecutb/data/TodoRepositoryImpl.java
@@ -1,76 +1,94 @@
 package se.ecutb.data;
 
+import se.ecutb.model.Person;
 import se.ecutb.model.Todo;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TodoRepositoryImpl implements TodoRepository {
+
+    private List<Todo> todoList = new ArrayList<>();
 
 
     @Override
     public Todo persist(Todo todo) {
-        return null;
+        if(!todoList.contains(todo)){
+            todoList.add(todo);
+        }
+        return todo;
     }
 
     @Override
     public Optional<Todo> findById(int todoId) {
-        return Optional.empty();
+        return todoList.stream().filter(todo -> todo.getTodoId() == todoId)
+                .findFirst();
     }
 
     @Override
     public List<Todo> findByTaskDescriptionContains(String taskDescription) {
-        return null;
+        return todoList.stream().filter(todo -> todo.getTaskDescription().equalsIgnoreCase(taskDescription))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findByDeadLineBefore(LocalDate endDate) {
-        return null;
+        return todoList.stream().filter(todo -> todo.getDeadLine().isBefore(endDate))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findByDeadLineAfter(LocalDate startDate) {
-        return null;
+        return todoList.stream().filter(todo -> todo.getDeadLine().isAfter(startDate))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findByDeadLineBetween(LocalDate start, LocalDate end) {
-        return null;
+        return todoList.stream().filter(todo -> todo.getDeadLine().equals(Period.between(start, end)))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findByAssigneeId(int personId) {
-        return null;
+        return todoList.stream().filter(todo -> todo.getAssignee().getPersonId() == personId)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findAllUnassignedTasks() {
-        return null;
+        return todoList.stream().filter(todo -> todo.getAssignee().equals(null))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findAllAssignedTasks() {
-        return null;
+        return todoList.stream().filter(todo -> todo.getAssignee() != null)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findByDone(boolean isDone) {
-        return null;
+        return todoList.stream().filter(todo -> todo.isDone() == isDone)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Todo> findAll() {
-        return null;
+        return todoList;
     }
 
     @Override
     public boolean delete(int todoId) throws IllegalArgumentException {
-        return false;
+        return todoList.remove(findById(todoId).orElseThrow(IllegalArgumentException::new));
     }
 
     @Override
     public void clear() {
-
+        todoList.clear();
     }
 }
